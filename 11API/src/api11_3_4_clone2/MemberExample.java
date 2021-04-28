d4a396374e514eec9ae4d5940659b8583e542259
JWP <jiwon9412@gmail.com>
2021-04-28 19:53:14 +0900

    [API] 11.3.5 finalize()

--- 11API/src/api11_3_4_clone2/MemberExample.java
+++ 11API/src/api11_3_4_clone2/MemberExample.java
@@ -7,7 +7,7 @@ public class MemberExample {
 		Member original = new Member("홍길동",25,new int[] {90,90}, new Car("소나타"));
 
 		
-		//복제 객체를 얻은 후에 패스워드 변경
+		//복제 객체를 얻은 후에 참조 객체의 값을 변경
 		Member cloned = original.getMember();
 		cloned.scores[0]=100;
 		cloned.car.model = "그랜져";
---
+++ 11API/src/api11_3_5_finalize/FinalizeExample.java
@@ -0,0 +1,13 @@
+package api11_3_5_finalize;
+
+public class FinalizeExample {
+	public static void main(String[] args) {
+		Counter counter = null;
+		for(int i = 1;i<=50;i++) {
+			counter = new Counter(i);
+			
+			counter = null;
+			System.gc();//Garbage Collector
+		}
+	}
+}
