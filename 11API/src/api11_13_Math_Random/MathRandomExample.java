054d7228837a4c24ac8fd6a0e247150c97f49d69
JWP <jiwon9412@gmail.com>
2021-05-02 19:32:16 +0900

    [API] 11.13

---
+++ 11API/src/api11_13_Math_Random/MathRandomExample.java
@@ -0,0 +1,11 @@
+package api11_13_Math_Random;
+
+public class MathRandomExample {
+
+	public static void main(String[] args) {
+		int num = (int)(Math.random()*6)+1;
+		System.out.println("ÁÖ»çÀ§ ´« : "+num);
+
+	}
+
+}
